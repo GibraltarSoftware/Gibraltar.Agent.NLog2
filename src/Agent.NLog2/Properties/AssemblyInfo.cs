aeb82fec77de5cd5f4f9458017ad2ddf6d7305bc
Kendall Miller <Kendall@Gibraltarsoftware.com>
2015-09-01 15:48:27 -0400

    Added NuGet package setup and updated to Loupe 4.0.

--- src/Agent.NLog2/Properties/AssemblyInfo.cs
+++ src/Agent.NLog2/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLog 2 Target")]
 [assembly: AssemblyDescription("Extends Loupe Agent for NLog 2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gibraltar Software Inc.")]
+[assembly: AssemblyCompany("Gibraltar Software, Inc.")]
 [assembly: AssemblyProduct("Loupe")]
-[assembly: AssemblyCopyright("Copyright © 2008-2014 Gibraltar Software Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2008-2015 Gibraltar Software, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.4.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("3.8.1.2710")]
+[assembly: AssemblyInformationalVersionAttribute("3.8.1.2710")]
